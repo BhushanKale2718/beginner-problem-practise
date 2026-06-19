378f052fc25fe6b0ceb00c8a9e66ed6734359194
Bhushan Kale <bhushankale2718@gmail.com>
2026-06-19 22:18:57 +0530

    solved even odd problem

--- normal.c
+++ normal.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
- int main() {
-     int a;
-     for (a = 0; a < 10; a++) {
-         printf("%d\n", a);
-     }  
+ int main(void)
+ {
+    int a, b;
+    printf(" First Number: ");
+    scanf("%d", &a);
+    printf(" Second Number: ");
+    scanf("%d", &b);
+    if (a >b)
+    {
+        printf(" %d is greater than %d\n", a, b);
+    }
+    else if (a < b)
+    {
+        printf(" %d is less than %d\n", a, b);
+    }
+    else
+    {
+        printf(" %d is equal to %d\n", a, b);
+    }
  }
+ 
